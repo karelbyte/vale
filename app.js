bf3dd21e9092f5b6e82c81faf5156a29a050279f
Damicela Peña <damicelapr@gmail.com>
2022-04-28 10:07:44 -0500

    secund Commmit 28/04/22

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
 const app = express();
+const body_parser = require('body-parser');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'dami',
+  password : 'dami'
+});
 
-app.use(express.static('publics'));
+connection.connect();
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname + '/views/index.html'))
+app.use(express.static("publics"));
+app.use(body_parser.urlencoded({extended:true}));
+
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname + "/views/index.html"));
+});
+
+app.post("/confirmacion", (request, response) => {
+   connection.query(`insert into invitadosdb.personas (nombre, asistencia) values ("${request.body.nombre}", 1)`)
+   response.redirect('/');
 });
 
 app.listen(3000, () => {
-  console.log("sevidor escuchando")
+  console.log("sevidor escuchando");
 });
--- publics/index.js
+++ publics/index.js
@@ -1,58 +1,55 @@
-/* const music = new Audio('malagueña.mp3');
-music.play()
-music.loop =true;
-
-
-const promise = music.play();
-
-console.log(promise) 
-
-if (promise !== undefined) {
-  promise.then(_ => {
-    // Autoplay started!
-  }).catch(error => {
-    // Autoplay was prevented.
-    // Show a "Play" button so that user can start playback.
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    center: true,
+    items: 1,
+    loop: true,
+    margin: 30,
+    nav: true,
   });
-} */
 
-var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
-  
-  function cuantosDias() {
-      let hoy = new Date();
-      console.log()
-      let fechaDeEvento = new Date("2022, 05, 21")
-  
-      fechaDeEvento.setHours(19, 00, 00, 00)
-      milSegundosEnUnDia = 1000 * 60 * 60 * 24
-  
-  
-      const difDias = (fechaDeEvento.getTime() - hoy.getTime()) / milSegundosEnUnDia
-      let diferenciaDia = Math.trunc(difDias).toString().padStart(2, "0")
-  
-  
-      let difHoras = (difDias - Math.trunc(difDias)) * 24
-      let diferenciaHoras = Math.trunc(difHoras).toString().padStart(2, "0")
-  
-      let difMinutos = (difHoras - Math.trunc(difHoras)) * 60
-      let difeneciaMinutos = Math.trunc(difMinutos).toString().padStart(2, "0")
-      let difSegundos = (difMinutos - Math.trunc(difMinutos)) * 60
-      let diferenciaSegundos = Math.trunc(difSegundos).toString().padStart(2, "0")
-  
-      let horaImprimible = `${diferenciaDia} ${diferenciaHoras} ${difeneciaMinutos} ${diferenciaSegundos}`
-  
-      if (fechaDeEvento > hoy) {
-          document.getElementById("black").innerHTML = horaImprimible
-      } else {
-          document.getElementById("black").innerHTML = "La fecha ya pasó"
-      }
+  const btnAaudio = document.getElementById("btn-audio");
+
+  const music = new Audio("./images/vals_mariposas.mp3");
+  music.loop = true;
+
+  btnAaudio.onclick = function () {
+    if (music.paused) {
+      music.play();
+    } else {
+      music.pause();
+    }
+  };
+});
+
+function cuantosDias() {
+  let hoy = new Date();
+  console.log();
+  let fechaDeEvento = new Date("2022, 05, 21");
+
+  fechaDeEvento.setHours(19, 00, 00, 00);
+  milSegundosEnUnDia = 1000 * 60 * 60 * 24;
+
+  const difDias =
+    (fechaDeEvento.getTime() - hoy.getTime()) / milSegundosEnUnDia;
+  let diferenciaDia = Math.trunc(difDias).toString().padStart(2, "0");
+
+  let difHoras = (difDias - Math.trunc(difDias)) * 24;
+  let diferenciaHoras = Math.trunc(difHoras).toString().padStart(2, "0");
+
+  let difMinutos = (difHoras - Math.trunc(difHoras)) * 60;
+  let difeneciaMinutos = Math.trunc(difMinutos).toString().padStart(2, "0");
+  let difSegundos = (difMinutos - Math.trunc(difMinutos)) * 60;
+  let diferenciaSegundos = Math.trunc(difSegundos).toString().padStart(2, "0");
+
+  let horaImprimible = `${diferenciaDia} ${diferenciaHoras} ${difeneciaMinutos} ${diferenciaSegundos}`;
+
+  if (fechaDeEvento > hoy) {
+    document.getElementById("black").innerHTML = horaImprimible;
+  } else {
+    document.getElementById("black").innerHTML = "La fecha ya pasó";
   }
-  
-  
-  const loadMyScript = () => {
-      setInterval(cuantosDias, 1000)
-  }
+}
+
+const loadMyScript = () => {
+  setInterval(cuantosDias, 1000);
+};
